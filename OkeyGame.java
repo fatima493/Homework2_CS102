37dc1463b9d1056eadd13666403b7318c91b395b
fatima493 <fatima.hajiyeva@ug.bilkent.edu.tr>
2025-03-02 11:56:48 +0300

    Update OkeyGame.java
    getLastDiscardedTile() method written

--- OkeyGame.java
+++ OkeyGame.java
@@ -56,8 +56,10 @@ public class OkeyGame {
      * it should return the toString method of the tile so that we can print what we picked
      */
     public String getLastDiscardedTile() {
-        
-        return null;
+        if (lastDiscardedTile != null) {
+            return lastDiscardedTile.toString();
+        }
+        return "No discarded tile yet.";
     }
 
     /*
